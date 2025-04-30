959f314b455605e093eebab85e4c0d916aeca71d
Wansoo Ha <pkgpl@users.noreply.github.com>
2025-04-30 06:16:44 +0000

    openai api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,18 @@
 import streamlit as st
-st.write("Hello World")
+from openai import openai
+
+api_key= st.text_input("OpenAI API Key", type="password")
+client = OpenAI(api_key=api_key)
+
+st.title("OpenAI GPT model")
+
+prompt = st.text_area("User prompt")
+
+if st.button("Ask!", disabled=(len(prompt)==0)):
+    response = client.responses.create(
+        model="gpt-4.1-mini",
+        input=prompt
+    )
+
+    st.write(response.output_text)
+    
