13cd31932359180fd4962f85d2a894cc642ff85d
Wansoo Ha <pkgpl@users.noreply.github.com>
2025-05-07 05:42:20 +0000

    cache data, session_state

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,27 @@
 import streamlit as st
 from openai import OpenAI
 
-api_key= st.text_input("OpenAI API Key", type="password")
-client = OpenAI(api_key=api_key)
-
-st.title("OpenAI GPT model")
-
-prompt = st.text_area("User prompt")
-
-if st.button("Ask!", disabled=(len(prompt)==0)):
+@st.cache_data
+def get_response(prompt):
     response = client.responses.create(
         model="gpt-4.1-mini",
         input=prompt
     )
+    return response.output_text
 
-    st.write(response.output_text)
+
+st.title("OpenAI GPT model")
+
+api_key= st.text_input("OpenAI API Key", type="password")
+if api_key:
+    st.session_state["OPENAI_API_KEY"] = api_key
+    client = OpenAI(api_key=api_key)
+    st.session_state["client"] = client
+else:
+    st.markdown("API KEY를 입력하세요.")
+    st.stop()
+
+prompt = st.text_area("User prompt")
+
+if st.button("Ask!", disabled=(len(prompt)==0)):
+    st.write(get_response(prompt))
